b7b3345b8db241feef5de65d941f2897046c2434
ssents <ssentumbweenoch@gmail.com>
2022-03-15 13:59:26 +0300

    Add Postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from sk.py import secret_key
+from backend.sk import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stonewell_tech',
+        'USER': 'postgres',
+        'PASSWORD':'jmonty56#',
+        'HOST':'localhost'
     }
 }
 
