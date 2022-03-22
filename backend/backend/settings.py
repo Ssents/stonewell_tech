4540d66a7bc00ba95b51e2c8cbed97468fdf06a3
ssents <ssentumbweenoch@gmail.com>
2022-03-22 10:37:00 +0300

    Add rest_framework and simple_jwt to backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # THIRD PARTY APPS
+
+    # CUSTOM APPS
+    'user',
 ]
 
 MIDDLEWARE = [
