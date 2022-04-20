8af71d9ee7ce394b88cbe5237cc735d71fbee732
ssents <ssentumbweenoch@gmail.com>
2022-04-20 06:24:11 +0300

    Add model to product, serial_number to variant

--- backend/product/models.py
+++ backend/product/models.py
@@ -5,7 +5,9 @@ class Product(models.Model):
     name = models.CharField(max_length=256, unique = True, blank = False)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
+    model = models.CharField(max_length=256)
 
 class Variant(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     mass = models.IntegerField()
+    serial_number = models.CharField(max_length=256, blank=False)
